950a7487e0f988bc7798efbd8b33d7f5ff9a8841
fancyblue <53809287+fancyblue@users.noreply.github.com>
2026-08-16 17:30:17 +0800

    feat: add Google OAuth configuration

---
+++ config.js
@@ -0,0 +1,7 @@
+window.STORYFLOW_CONFIG = {
+  googleClientId: "806756717305-kp8ocka22c1nh0gk32iukvpj0p9o972n.apps.googleusercontent.com",
+  googleScopes: [
+    "https://www.googleapis.com/auth/documents.readonly",
+    "https://www.googleapis.com/auth/drive.file"
+  ]
+};
