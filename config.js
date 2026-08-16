6e05259df875480e13e2729c3628cc9a5748dec1
fancyblue <53809287+fancyblue@users.noreply.github.com>
2026-08-16 17:41:13 +0800

    chore: add Google Cloud project number

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.STORYFLOW_CONFIG = {
   googleClientId: "806756717305-kp8ocka22c1nh0gk32iukvpj0p9o972n.apps.googleusercontent.com",
+  googleProjectNumber: "806756717305",
   googleScopes: [
     "https://www.googleapis.com/auth/documents.readonly",
     "https://www.googleapis.com/auth/drive.file"
